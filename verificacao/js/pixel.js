63b1a0952723967ace7398750aa21056715c1488
CampanhaSocial2025 <felipemartisn75@gmail.com>
2025-08-13 03:51:07 -0300

    Add files via upload

---
+++ verificacao/js/pixel.js
@@ -0,0 +1,21 @@
+(function () {
+window.pixelId = "6890cdb872ae589829484c12";
+  var a = document.createElement("script");
+  a.setAttribute("async", "");
+  a.setAttribute("defer", "");
+  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+  document.head.appendChild(a);
+
+})();
+
+(function () {
+  const script = document.createElement("script");
+
+  script.src = "https://cdn.utmify.com.br/scripts/utms/latest.js";
+  script.setAttribute("data-utmify-prevent-xcod-sck", "");
+  script.setAttribute("data-utmify-prevent-subids", "");
+  script.async = true;
+  script.defer = true;
+
+  document.head.appendChild(script);
+})();
